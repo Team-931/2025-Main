7f477f2bcb346b37f037054a0782258c091d4de9
A564790 <catsandogs3300@gmail.com>
2025-03-09 16:09:04 -0500

    Added the wrist and controls, updated elevator

--- src/main/java/frc/robot/Subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/Subsystems/AlgaeIntake.java
@@ -18,7 +18,6 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
