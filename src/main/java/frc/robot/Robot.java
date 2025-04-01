8700707175385c92cb3c5faa41e10f1f1f98a541
Philip Harrington <71712.2415@compuserve.com>
2025-04-01 15:17:37 -0600

    Both Limelights report on Dashboard (for testing)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -22,6 +23,10 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("left targets", LimelightHelpers.getTargetCount("limelight-left"));
+    SmartDashboard.putNumber("left id", LimelightHelpers.getFiducialID("limelight-left"));
+    SmartDashboard.putNumber("right targets", LimelightHelpers.getTargetCount("limelight-right"));
+    SmartDashboard.putNumber("right id", LimelightHelpers.getFiducialID("limelight-right"));
   }
 
   @Override
